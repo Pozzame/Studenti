b5b0e83ef040c6a57d2cb7459fcd91d29e4de57a
frasabbi <fra.sabbi@gmail.com>
2018-12-04 14:45:04 +0100

    b

--- Studenti/Program.cs
+++ Studenti/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Studenti
 {
     class Program
     {
+        static public IList<Student> School = new List <Student>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            using (var reader = new StreamReader(@"C:\Users\Allievo 3\source\repos\Studenti\Studenti\Studenti.csv"))
+            {
+                //List<string> listA = new List<string>();
+                //List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    School.Add(new Student(Convert.ToInt32(values[0]), values[1], values[2], Convert.ToInt32(values[3]), Convert.ToChar(values[4]), Convert.ToInt32(values[5])));
+                }
+            }
+
+            Console.WriteLine("Enter command: (? for Help)");
+            var input = Console.Read();
+            switch (input)
+            {
+                case '?':
+                    Console.WriteLine("L = List all Students;\nF = Search for ID;\nV = Valore medio voti degli studenti;\nM = Moda dei voti degli studenti;\nD = Mediana dei voti degli studenti;");
+                    break;
+                case 'l':
+                case 'L':
+                    foreach (var Student in School)
+                    {
+                        Console.WriteLine(Student.ToString());
+                    }
+                    break;
+                case 'f':
+                case 'F':
+                    Console.WriteLine("Insert ID:");
+                    var key = (Console.Read());
+
+                    foreach (var Student in School)
+                    {
+                        if (Student.ID == key)
+                        {
+                            Console.WriteLine(Student.ToString());
+                            break;
+                        }
+                    }
+                    Console.WriteLine("Student not found...");
+                    break;
+                default:
+                    { break; }
+
+
+
+                    //Student Pippo = new Student(1, "Pippo", "Pasticcio", 23, 'M', 5);
+                    //Console.WriteLine(Pippo.ToString());
+            }
+        } 
+   }
 }
---
+++ Studenti/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Studenti
+{
+    public class Student
+    {
+        public int ID { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        public char Gender  { get; private set; }
+        public int Vote { get; private set; }
+
+        public Student(int ID, string Name, string Surname, int Age, char Gender, int Vote)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Age = Age;
+            this.Gender = Gender;
+            this.Vote = Vote;
+        }
+
+        public override string ToString()
+        {
+            return ID + " " + Name + " " + Surname + " " + Age + " " + Gender + " " + Vote;
+        }
+    }
+}
